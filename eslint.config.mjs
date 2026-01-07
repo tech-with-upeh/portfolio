c1c6170b3cbe940c6fd075a22926091a618775f6
Upeh <wisdomupeh@gmail.com>
2026-01-07 15:21:18 +0100

    eslint final fix!

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,3 @@
-import { defineConfig, globalIgnores } from 'eslint/config'
-import nextVitals from '@eslint-config-next/core-web-vitals'
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
@@ -11,23 +9,8 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
- 
-const eslintConfig = defineConfig([
-  ...nextVitals,
-  {
-    rules: {
-      'react/no-unescaped-entities': 'off',
-      '@next/next/no-page-custom-font': 'off',
-    },
-  },
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    '.next/**',
-    'out/**',
-    'build/**',
-    'next-env.d.ts',
-  ]),
-])
- 
-export default eslintConfig
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+];
+
+export default eslintConfig;
